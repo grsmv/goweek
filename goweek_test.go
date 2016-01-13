f8e3e91ced365b220efa5ff548be0cbc0135d715
Serhii Herasymov <serhiiherasymov@gmail.com>
2016-01-13 17:35:50 +0200

    fixing and closing #4, #5

--- goweek_test.go
+++ goweek_test.go
@@ -7,16 +7,6 @@ import (
 )
 
 var expectedDays = []time.Time{
-	time.Date(2015, 11, 8, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 9, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 10, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 11, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 12, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 13, 0, 0, 0, 0, time.UTC),
-	time.Date(2015, 11, 14, 0, 0, 0, 0, time.UTC),
-}
-
-var expectedDaysWithMondayAsFirstDayOfTheWeek = []time.Time {
 	time.Date(2015, 11, 9, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 10, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 11, 0, 0, 0, 0, time.UTC),
@@ -27,45 +17,44 @@ var expectedDaysWithMondayAsFirstDayOfTheWeek = []time.Time {
 }
 
 var expectedDaysForNextWeek = []time.Time {
-	time.Date(2015, 11, 15, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 16, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 17, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 18, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 19, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 20, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 21, 0, 0, 0, 0, time.UTC),
+	time.Date(2015, 11, 22, 0, 0, 0, 0, time.UTC),
 }
 
 // assuming that in case of 2015 year last week of 2015 in first week of 2016
 var expectedDaysForNextWeekWithYearSwitch = []time.Time{
-	time.Date(2016, 1,  3,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  4,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  5,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  6,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  7,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  8,  0, 0, 0, 0, time.UTC),
 	time.Date(2016, 1,  9,  0, 0, 0, 0, time.UTC),
+	time.Date(2016, 1,  10, 0, 0, 0, 0, time.UTC),
 }
 
 var expectedDaysForPreviousWeek = []time.Time{
-	time.Date(2015, 11, 1, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 2, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 3, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 4, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 5, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 6, 0, 0, 0, 0, time.UTC),
 	time.Date(2015, 11, 7, 0, 0, 0, 0, time.UTC),
+	time.Date(2015, 11, 8, 0, 0, 0, 0, time.UTC),
 }
 
-// assuming that in case of 2015 year first week of 2015 in last week of 2014
 var expectedDaysForPreviousWeekWithYearSwitch = []time.Time{
-	time.Date(2014, 12, 21, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 22, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 23, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 24, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 25, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 26, 0, 0, 0, 0, time.UTC),
-	time.Date(2014, 12, 27, 0, 0, 0, 0, time.UTC),
+	time.Date(2014, 12, 29, 0, 0, 0, 0, time.UTC),
+	time.Date(2014, 12, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(2014, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(2015,  1,  1, 0, 0, 0, 0, time.UTC),
+	time.Date(2015,  1,  2, 0, 0, 0, 0, time.UTC),
+	time.Date(2015,  1,  3, 0, 0, 0, 0, time.UTC),
+	time.Date(2015,  1,  4, 0, 0, 0, 0, time.UTC),
 }
 
 func Test_NormalUsage(t *testing.T) {
@@ -79,21 +68,8 @@ func Test_NormalUsage(t *testing.T) {
 	if week.Number != 46 {
 		t.Error("Unexpected Week.Number, \n expected %v, \n given %v", 46, week.Number)
 	}
-	if week.FirstDay != 0 {
-		t.Error("Unexpected Week.FirstDay, \n expected %v, \n given %v", 0, week.FirstDay)
-	}
 	if !reflect.DeepEqual(expectedDays, week.Days) {
 		t.Errorf("Unexpected Week.Days, \n expected %v, \n given %v", expectedDays, week.Days)
-	}
-}
-
-func Test_MondayAsFirstWeekDay(t *testing.T) {
-	var week, _ = NewWeek(2015, 46, 1)
-	if week.FirstDay != 1 {
-		t.Error("Unexpected Week.FirstDay, \n expected %v, \n given %v", 1, week.FirstDay)
-	}
-	if !reflect.DeepEqual(expectedDaysWithMondayAsFirstDayOfTheWeek, week.Days) {
-		t.Errorf("Unexpected Week.Days, \n expected %v, \n given %v", expectedDaysWithMondayAsFirstDayOfTheWeek, week.Days)
 	}
 }
 
@@ -155,32 +131,12 @@ func Test_PreviousWeek (t *testing.T) {
 	}
 }
 
-func Test_YearsVerge (t *testing.T) {
-	var weekA, _ = NewWeek(2015, 53, 1)
-	var nextWeek, _ = weekA.Next()
-	if nextWeek.Number != 2 {
-		t.Errorf("Unexpected Week.Next() at the verge of years, \n expected %v, \n given %v", 2, nextWeek.Number)
-		t.Errorf("expected %v, \n %v", weekA.Days, nextWeek.Days)
-	}
+func Test_ISOWeek_Compatibility (t *testing.T) {
+	var week, _ = NewWeek(2016, 1)
+	var w, y = week.Days[0].ISOWeek()
+	var weekDay = week.Days[0].Weekday()
 
-	var weekB, _ = NewWeek(2015, 1, 1)
-	var previousWeek, _ = weekB.Previous()
-	if previousWeek.Number != 52 {
-		t.Errorf("Unexpected Week.Previous() at the verge of years, \n expected %v, \n given %v", 52, previousWeek.Number)
-		t.Errorf("expected %v, \n %v", weekB.Days, previousWeek.Days)
-	}
-}
-
-func Test_YearsVergeWithoutNextAndPreviousWeekFixing (t *testing.T) {
-	var weekA, _ = NewWeek(2016, 53)
-	var nextWeek, _ = weekA.Next()
-	if nextWeek.Number != 1 {
-		t.Errorf("Unexpected Week.Next() at the verge of years (without fixing), \n expected %v, \n given %v", 1, nextWeek.Number)
-	}
-
-	var weekB, _ = NewWeek(2017, 1)
-	var previousWeek, _ = weekB.Previous()
-	if previousWeek.Number != 53 {
-		t.Errorf("Unexpected Week.Previous() at the verge of years (without fixing), \n expected %v, \n given %v", 53, previousWeek.Number)
+	if w != 2016 && y != 1 && weekDay != 3 {
+		t.Error("Broken ISO8601 compatibility")
 	}
 }
